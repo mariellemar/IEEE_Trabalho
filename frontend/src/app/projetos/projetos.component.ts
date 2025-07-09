8e8b045b84c7dd8031d1433984376a05e0aa52f2
ViniMorei <vinicius.moreira.ara@gmail.com>
2025-07-09 15:48:39 -0400

    feat: Include feedback and confirmation dialog

--- frontend/src/app/projetos/projetos.component.ts
+++ frontend/src/app/projetos/projetos.component.ts
@@ -3,21 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProjetosService } from '../services/projetos.service';
 import { ProjetosMockService } from '../services/projetos.mock.service';
+import { Dialog } from '../dialog/dialog.component';
 
 @Component({
   selector: 'projetos',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, Dialog],
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.scss'
 })
 
 export class Projetos {
-  editing: boolean = false;
+  editing = false;
   editID: number | null = null;
+  deleteID: number | null = null;
 
   analises: any[] = [1, 2, 3, 4];
   items: any[] = [];
   // nextID = 1;
+
+  showDialog = false;
+  dialogTitle = "";
+  dialogMessage = "";
+  dialogType: "confirm" | "alert" = "alert";
 
   newItem = {
     analise : "",
@@ -35,6 +42,14 @@ export class Projetos {
   }
 
   onSubmit() {
+    if (this.invalidForms()) {
+      this.dialogTitle = "Formulário incompleto";
+      this.dialogMessage = "Por favor, preencha todos os campos antes de enviar.";
+      this.dialogType = "alert";
+      this.showDialog = true;
+      return;
+    }
+
     if (this.editing && this.editID !== null) {
       const updatedItem = { id: this.editID, ...this.newItem };
       
@@ -51,8 +66,12 @@ export class Projetos {
       });
 
     };
-
     this.resetForms();
+
+    this.dialogTitle = "Sucesso";
+    this.dialogMessage = "Projeto salvo com sucesso!";
+    this.dialogType = "alert";
+    this.showDialog = true;
   };
 
 
@@ -63,10 +82,18 @@ export class Projetos {
   };
 
 
-  deleteItem(id: number) {
-    this.projetoService.deleteProjeto(id).subscribe(() => {
-      this.items = this.items.filter(item => item.id !== id);
-    });
+  deleteItem() {
+    if (this.deleteID !== null) {
+      console.log(this.deleteID);
+      this.projetoService.deleteProjeto(this.deleteID).subscribe(() => {
+        this.items = this.items.filter(item => item.id !== this.deleteID);
+
+        this.projetoService.getProjetos().subscribe(data => this.items = data);
+      });
+
+      this.deleteID = null;
+    };
+    this.closeFeedback();
   };
 
   resetForms() {
@@ -81,5 +108,39 @@ export class Projetos {
 
     this.editing = false;
     this.editID = null;
+  }
+
+  invalidForms(): boolean {
+    return (
+      !this.newItem.analise ||
+      !this.newItem.descricao ||
+      !this.newItem.inicio ||
+      !this.newItem.final ||
+      !this.newItem.responsavel ||
+      !this.newItem.status
+    );
+  }
+
+  confirmDelete(id: number) {
+    this.deleteID = id;
+    this.dialogTitle = `Tem certeza que deseja apagar o projeto com o id ${this.deleteID}?`;
+    this.dialogMessage = "Esta ação não pode ser revertida!";
+    this.dialogType = "confirm";
+    this.showDialog = true;
+  }
+
+  onDialogConfirm() {
+    if (this.dialogType === "confirm") {
+      this.deleteItem();
+    }
+    this.closeFeedback();
+  }
+
+  closeFeedback() {
+    this.showDialog = false;
+    this.dialogType = 'alert';
+    this.dialogTitle = '';
+    this.dialogMessage = '';
+    this.deleteID = null;
   }
 }
